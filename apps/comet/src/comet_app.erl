990056040f81655fd1c3e896289b462ba6f76bf5
Max Lapshin <max@maxidoors.ru>
2012-08-29 23:17:02 +0400

    Now can start cowboy and show files from www/

--- apps/comet/src/comet_app.erl
+++ apps/comet/src/comet_app.erl
@@ -17,7 +17,34 @@ start() ->
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    comet_sup:start_link().
+  ok = application:start(mimetypes),
+  
+  Routes = [
+    {[<<"comet">>], comet_handler, []},
+    {[], cowboy_http_static, [
+      {directory, <<"www">>},
+      {file, <<"index.html">>},
+      {mimetypes,[{<<".html">>,[<<"text/html">>]}]}
+    ]},
+    {['...'], cowboy_http_static, [
+      {directory, <<"www">>},
+      {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+    ]}
+  ],
+  Dispatch = [{'_', Routes}],
+  HTTPPort = 8080,
+
+
+  ProtoOpts = [{dispatch, Dispatch},{max_keepalive,4096}],
+
+  ok = application:start(cowboy),
+  
+  {ok, _Listener} = cowboy:start_listener(comet_http, 100, 
+    cowboy_tcp_transport, [{port,HTTPPort},{backlog,4096},{max_connections,8192}],
+    cowboy_http_protocol, ProtoOpts
+  ),
+  
+  comet_sup:start_link().
 
 stop(_State) ->
     ok.
